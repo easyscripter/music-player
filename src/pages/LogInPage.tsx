75e11944e8a33dfad724780a693e8db01f7d3339
easyscripter <deniscs49@gmail.com>
2023-05-01 23:04:09 +0300

    feat: add  vk button to login page

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,9 +1,19 @@
 import { StyledLoginPage } from "../styles/components/StyledLoginPage";
+import { VKAuthButton } from '../styles/components/VKAuthButton';
 
 const LogInPage = () => {
+
+    const logInVK = () => {
+        console.log('test');
+    }
     return (
         <StyledLoginPage.Wrapper>
             <StyledLoginPage.Form>
+                <VKAuthButton.Container onClick={logInVK}>
+                    <VKAuthButton.Text>
+                       Войти через VK
+                    </VKAuthButton.Text>
+                </VKAuthButton.Container>
             </StyledLoginPage.Form>
         </StyledLoginPage.Wrapper>
     )
